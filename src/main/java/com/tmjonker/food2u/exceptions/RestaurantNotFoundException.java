a5b8d60a456853ce271c34c76abd1c84a04faff1
Tim Jonker <tmjonker1@outlook.com>
2022-06-02 22:05:13 -0400

    Removed the REST api

--- src/main/java/com/tmjonker/food2u/exceptions/RestaurantNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.tmjonker.food2u.exceptions;
-
-public class RestaurantNotFoundException extends RuntimeException {
-
-    public RestaurantNotFoundException(Integer id) {
-
-        super("Could not find restaurant " + id);
-    }
-}
